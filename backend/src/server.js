7c13643448d0b26e86eb00a06ca474e8b1847614
GABER77 <mg7965724@gmail.com>
2025-07-27 18:17:50 +0300

    feat: add MongoDB connection setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,10 @@
 import './config/loadEnv.js';
 import app from './app.js';
+import connectDB from './config/mongodb.js';
+
+await connectDB();
 
 const port = process.env.PORT || 8000;
-
 const server = app.listen(port, () =>
   console.log(`🚀 App running on port ${port}`)
 );
