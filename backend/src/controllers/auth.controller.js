2ff9a94e7c3b8f27cc63217a25c341d0aba888a1
GABER77 <mg7965724@gmail.com>
2025-07-30 18:01:13 +0300

    Implement user login with credential validation and token response

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import User from '../models/user.model.js';
 import catchAsync from '../utils/catchAsync.js';
 import filterObject from '../utils/filterObject.js';
 import createSendToken from '../utils/token.js';
+import CustomError from '../utils/customError.js';
 
 const signUp = catchAsync(async (req, res, next) => {
   // Filter only allowed fields from the request body
@@ -21,8 +22,24 @@ const signUp = catchAsync(async (req, res, next) => {
   createSendToken(newUser, 201, res);
 });
 
+const login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new CustomError('Please enter your email and password', 400);
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user || !(await user.checkPassword(password, user.password))) {
+    throw new CustomError('Invalid Credentials', 401);
+  }
+
+  createSendToken(user, 200, res);
+});
+
 const authController = {
   signUp,
+  login,
 };
 
 export default authController;
