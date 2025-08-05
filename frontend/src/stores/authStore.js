77847a878e5e7bbb372a2cd9dc75fe36b608db31
GABER77 <mg7965724@gmail.com>
2025-08-05 22:54:41 +0300

    Add logout functionality for frontend

--- frontend/src/stores/authStore.js
+++ frontend/src/stores/authStore.js
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
 
 export const authStore = create((set) => ({
   authUser: null,
@@ -28,6 +29,16 @@ export const authStore = create((set) => ({
       throw error; // So the component can catch and show error message
     } finally {
       set({ isSigningUp: false });
+    }
+  },
+
+  logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged out successfully");
+    } catch (error) {
+      toast.error(error.response.data.data.message);
     }
   },
 }));
